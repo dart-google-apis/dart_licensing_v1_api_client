e5de47674e17b2f3ec53ce0913a5d35e8d2a8ee0
Adam Singer <financeCoding@gmail.com>
2014-09-13 18:54:34 -0700

    Automated update

--- lib/licensing_v1_api_browser.dart
+++ lib/licensing_v1_api_browser.dart
@@ -6,6 +6,7 @@ import 'package:google_licensing_v1_api/src/browser_client.dart';
 import "package:google_licensing_v1_api/licensing_v1_api_client.dart";
 
 /** Licensing API to view and manage license for your domain. */
+@deprecated
 class Licensing extends Client with BrowserClient {
 
   final oauth.OAuth2 auth;
--- lib/licensing_v1_api_console.dart
+++ lib/licensing_v1_api_console.dart
@@ -7,6 +7,7 @@ import 'package:google_licensing_v1_api/src/console_client.dart';
 import "package:google_licensing_v1_api/licensing_v1_api_client.dart";
 
 /** Licensing API to view and manage license for your domain. */
+@deprecated
 class Licensing extends Client with ConsoleClient {
 
   final oauth2.OAuth2Console auth;
